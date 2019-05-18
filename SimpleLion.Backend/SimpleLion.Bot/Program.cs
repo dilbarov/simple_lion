e6191e8e95c5eedfd8214047881a80e72c7b7940
kolalola <kolalola777@gmail.com>
2019-05-18 18:03:36 +0500

    bot init

---
+++ SimpleLion.Backend/SimpleLion.Bot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using MihaZupan;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace SimpleLion.Bot
+{
+    internal class Program
+    {
+        private static ITelegramBotClient _botClient;
+
+        private static void Main(string[] args)
+        {
+            _botClient = new TelegramBotClient(Environment.GetEnvironmentVariable("BotToken"));
+
+            _botClient.OnMessage += OnMessage;
+            _botClient.StartReceiving();
+            var me = _botClient.GetMeAsync().Result;
+
+            Console.WriteLine("Bot is started");
+            Thread.Sleep(int.MaxValue);
+        }
+
+        private static async void OnMessage(object sender, MessageEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Message.Text))
+                await _botClient.SendTextMessageAsync(
+                    e.Message.Chat,
+                    "Hello");
+        }
+    }
+}
