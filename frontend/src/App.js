1f4d633bc5028f5cf917c432bffcf818f7b336b3
guzaev <a.guzaev95@gmail.com>
2019-05-19 03:01:29 +0500

    add map, list and etc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.get = this.get
+    this.state = {
+      test: 'old'
+    }
+  }
+  get() {
+    axios.get('http://localhost:3002/getUser')
+      .then(res => {
+        this.setState({ test: res.data });
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  }
+  componentDidMount() {
+    this.get()
+  }
+  success(position) {
+    console.log(position.coords);
+  }
+  render() {
+    return (
+      <div className="App">
+        { navigator.geolocation.getCurrentPosition(pos => console.log(pos.coords)) }
+        Hi !!! 
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/component/event/Event.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { List } from 'semantic-ui-react'
+import './Event.css';
+
+class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.body = this.body.bind(this);
+    this.openBody = this.openBody.bind(this);
+    this.state = {
+      open: false
+    };
+  }
+  openBody() {
+    this.setState({ open: !this.state.open })
+  }
+  body() {
+    if(this.state.open) {
+      return(
+        <List.Description>
+          <div className="event-comment">{ this.props.comment }</div>
+          <div className="event-date">{ this.props.startTime }</div>
+          <div className="event-location">{ this.props.locationName }</div>
+        </List.Description>
+      )
+    }
+  }
+  render() {
+    return(
+      <div className="event-main">
+        <List.Item onClick={ this.openBody }>
+          <List.Header>{ this.props.title }</List.Header>
+          { this.body() }
+        </List.Item>
+      </div>
+    );
+  }
+}
+
+export default Event;
